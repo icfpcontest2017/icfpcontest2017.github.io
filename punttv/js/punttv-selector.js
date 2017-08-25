dbfbbbc1ab7b61b8828212be89fc454481665281
sam <Sam.Lindley@ed.ac.uk>
2017-08-25 17:33:33 +0100

    update punttv-selector.js

--- punttv/js/punttv-selector.js
+++ punttv/js/punttv-selector.js
@@ -1,14 +1,16 @@
-const summary_file = "/punttv/summary.json"
+const summary_of_summaries_file = "summary-of-summaries.json";
+
+let selected_round = 0;
+
 
 function filteredSearch() {
-    var i;
-    var phrase = document.getElementById("phrase");
-    var filter = phrase.value.toUpperCase();
-    var listing = document.getElementById("playerTable");
-    var teams = listing.getElementsByClassName("teamRow");
-    for (i = 0; i < teams.length; i++) {
-        var team = teams[i];
-        var teamName = team.getElementsByClassName("teamName")[0];
+    const phrase = document.getElementById("phrase");
+    const filter = phrase.value.toUpperCase();
+    const listing = document.getElementById("playerTable" + selected_round.toString());
+    const teams = listing.getElementsByClassName("teamRow");
+    for (let i = 0; i < teams.length; i++) {
+        const team = teams[i];
+        const teamName = team.getElementsByClassName("teamName")[0];
         if (teamName !== undefined) {
             if (teamName.innerHTML.toUpperCase().indexOf(filter) > -1) {
                 teams[i].style.display = "";
@@ -23,8 +25,33 @@ function capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-function displaySummaryTable(json) {
-  const tbl = document.getElementById("playerTable");
+function initTableHeader(tbl) {
+  const thead = document.createElement("thead");
+  const tr = document.createElement("tr");
+  const th = document.createElement("th");
+  const txt = document.createTextNode("Team");
+  th.appendChild(txt);
+  tr.appendChild(th);
+  thead.appendChild(tr);
+  tbl.appendChild(thead);
+
+}
+
+function displaySummaryTable(round_num, json) {
+  // Create the table element
+  const tbl_container = document.getElementById("tab-content-container");
+  const content_div = document.createElement("div");
+  content_div.setAttribute("id", "round" + round_num.toString());
+  content_div.classList.add("tab-pane");
+  if (round_num == 0) {
+    content_div.classList.add("in");
+    content_div.classList.add("active");
+  }
+  const tbl = document.createElement("table");
+  tbl.setAttribute("id", "playerTable" + round_num.toString());
+  tbl.classList.add("player-table");
+  initTableHeader(tbl);
+
   json.game.forEach(game => {
     const tr = document.createElement('tr');
     tr.classList.add('teamRow');
@@ -46,7 +73,7 @@ function displaySummaryTable(json) {
             categories.style.display = 'none';
     }
 
-    teamName.addEventListener('click', clickHandler);
+    tr.addEventListener('click', clickHandler);
 
     Object.keys(game).forEach(key => {
         if (key != "team") {
@@ -62,9 +89,9 @@ function displaySummaryTable(json) {
                 if (cur !== splitFile[0]) {
                     cur = splitFile[0];
                     i = i + 1;
-                    games.push({'map': cur, 'heats':[]});
+                    games.push({'map': cur, 'plays':[]});
                 }
-                games[i].heats.push({'ref': "tv.html?game=" + gameName, 'number': num});
+                games[i].plays.push({'ref': "tv.html?game=" + gameName, 'number': num});
             });
 
             // Layout the subcategory
@@ -83,16 +110,16 @@ function displaySummaryTable(json) {
                 row.appendChild(map_col);
                 row.append(document.createElement("td"));
 
-                const num_heats = game.heats.length;
-                for (var j = 0; j < num_heats; j++) {
-                    const heat_col = document.createElement("td");
-                    const heat = game.heats[j];
+                const num_plays = game.plays.length;
+                for (var j = 0; j < num_plays; j++) {
+                    const play_col = document.createElement("td");
+                    const play = game.plays[j];
                     const a = document.createElement("a");
-                    a.setAttribute("href", heat.ref);
-                    const number = heat.number;
+                    a.setAttribute("href", play.ref);
+                    const number = play.number;
                     a.appendChild(document.createTextNode(number));
-                    heat_col.appendChild(a);
-                    row.appendChild(heat_col);
+                    play_col.appendChild(a);
+                    row.appendChild(play_col);
                 }
                 enumeration.appendChild(row);
             }
@@ -106,11 +133,50 @@ function displaySummaryTable(json) {
     tr.appendChild(td);
     tbl.appendChild(tr);
   });
+
+  content_div.appendChild(tbl);
+  tbl_container.appendChild(content_div);
+}
+
+function fetchJson(file, cb) {
+  fetch("/punttv/" + file, { mode: 'no-cors' })
+    .then(res => res.json())
+    .then(json => cb(json))
+}
+
+function loadSummary(round_id, summary_file) {
+  fetchJson(summary_file, (json) =>
+            { displaySummaryTable(round_id, json)});
 }
 
 
-function loadSummary() {
-  fetch(summary_file, { mode: 'no-cors' })
-    .then(res => res.json())
-    .then(json => displaySummaryTable(json))
+function createTabs(sos_json) {
+  let index = 0;
+  sos_json["rounds"].forEach( (round) => {
+      const li = document.createElement("li");
+      const a = document.createElement("a");
+      const idx = index;
+      a.setAttribute("data-toggle", "tab");
+      a.setAttribute("href", "#round" + index.toString());
+      a.addEventListener("click", function() {
+        selected_round = idx;
+        filteredSearch();
+      });
+      const txt = document.createTextNode(round.name);
+      a.appendChild(txt);
+      li.appendChild(a);
+      document.getElementById("nav-ul").appendChild(li);
+      loadSummary(index, round.file);
+      index++;
+    }
+  );
+}
+
+
+function loadSummaries() {
+  fetchJson(summary_of_summaries_file, createTabs);
+}
+
+function main() {
+  loadSummaries();
 }
